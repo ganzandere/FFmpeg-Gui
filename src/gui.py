ff71427fb14482c98271f6961d8dcf96d33098d3
Sibin <acimovic.sibin@gmail.com>
2023-04-05 04:53:13 +0200

    started implementing fill methods in gui and polishing ther rest

--- src/gui.py
+++ src/gui.py
@@ -1,5 +1,6 @@
 """Module defining GUI"""
 import os
+import tempfile
 from tkinter import filedialog as fd
 
 import customtkinter as ctk
@@ -11,6 +12,7 @@ from utils import (find_ffmpeg, sequence_collector, sequence_step_calc,
 
 class App(ctk.CTk):
     """Defines a GUI class."""
+
     def __init__(self):
         super().__init__()
 
@@ -85,7 +87,7 @@ class App(ctk.CTk):
         self.out_path_frame = ctk.CTkFrame(master=self, fg_color="transparent")
         self.out_path_frame.grid(row=5, column=0, padx=10, pady=5, sticky='w')
         self.output_entry = ctk.CTkEntry(master=self.out_path_frame, font=self.font,
-                                           placeholder_text="Output filename: ", corner_radius=c.CORNER_RADIUS, width=c.ENTRY_WIDTH)
+                                         placeholder_text="Output filename: ", corner_radius=c.CORNER_RADIUS, width=c.ENTRY_WIDTH)
         self.output_entry.grid(row=0, column=0, padx=10, pady=5)
         self.out_path_button = ctk.CTkButton(master=self.out_path_frame, text='Browse',
                                              command=self.output_browse_callback, corner_radius=c.BTN_RADIUS, font=self.font)
@@ -110,9 +112,17 @@ class App(ctk.CTk):
         self.containers_label = ctk.CTkLabel(master=self.run_subframe1, text='Container ', font=self.font)
         self.containers_opt.grid(row=0, column=0, padx=10, pady=5)
         self.containers_label.grid(row=0, column=1, padx=10, pady=5)
+        # Fill Method
+        self.fill_frame = ctk.CTkFrame(master=self.run_frame)
+        self.fill_frame.grid(row=0, column=1, padx=5)
+        self.fill_method_val = ctk.StringVar(value='Color')
+        self.fill_methods = ctk.CTkOptionMenu(master=self.fill_frame, values=c.FILL_METHODS, variable=self.fill_method_val, width=c.OPT_WIDTH, font=self.font)
+        self.fill_methods.grid(row=0, column=0, padx=5, pady=5)
+        self.fill_label = ctk.CTkLabel(master=self.fill_frame, text='Fill Method', font=self.font)
+        self.fill_label.grid(row=0, column=1, padx=5, pady=5)
         # Run Subframe2
         self.run_subframe2 = ctk.CTkFrame(master=self.run_frame)
-        self.run_subframe2.grid(row=0, column=1, padx=5)
+        self.run_subframe2.grid(row=0, column=2, padx=5)
         self.preview_button = ctk.CTkButton(master=self.run_subframe2, text='Preview', font=self.font, command=self.preview_callback)
         self.preview_button.grid(row=0, column=0, padx=10, pady=5)
         self.run_button = ctk.CTkButton(master=self.run_subframe2, text='Run', font=self.font, command=self.run_callback)
@@ -143,60 +153,65 @@ class App(ctk.CTk):
 
     def output_browse_callback(self):
         """Defines the behaviour of output path browse button."""
-        folder = fd.asksaveasfilename(filetypes=((f"{self.containers_opt.get()}", f"*.{self.containers_opt.get()}"),("All Files", "*.*") ))
+        folder = fd.asksaveasfilename(filetypes=((f"{self.containers_opt.get()}", f"*.{self.containers_opt.get()}"), ("All Files", "*.*")))
         self.output_entry.delete(0, "end")
         self.output_entry.insert(0, folder)
 
     def preview_callback(self):
-        """Constructs a preview of command in log"""
-        outfile, command = self.construct_command()
-        self.log_textbox.insert("end", f"\nConstructed ffmpeg command: \n{command}\n")
-        #Cleanup outfile after 
-        if os.path.isfile(outfile):
-            os.remove(outfile)
-
+        """Constructs a preview of command in Log"""
+        with tempfile.TemporaryDirectory() as temp_dir:
+            command = self.construct_command(temp_dir)
+            #Log output
+            self.log_textbox.delete("0.0", "end")
+            self.log_textbox.insert("0.0", f"\nConstructed ffmpeg command: \n{command}\n")
+        
     def run_callback(self):
         """Sends constructed command to ffmpeg."""
-        outfile, command = self.construct_command()
-        self.log_textbox.delete("0.0", "end")
-        self.log_textbox.insert("0.0", f"\nConstructed ffmpeg command: \n{command}\n\n")
-        stdout, stderr = submit_ffmpeg(command)
-        self.log_textbox.insert("end", f"FFmpeg output: {stderr}")
-        #Cleanup outfile after running ffmpeg.exe"
-        if os.path.isfile(outfile):
-            os.remove(outfile)
+        with tempfile.TemporaryDirectory() as temp_dir:
+            command = self.construct_command(temp_dir)
+            #Log output
+            self.log_textbox.delete("0.0", "end")
+            self.log_textbox.insert("0.0", f"\nConstructed ffmpeg command: \n{command}\n\n")
+            stdout, stderr = submit_ffmpeg(command)
+            self.log_textbox.insert("end", f"FFmpeg output: {stderr}")
 
-    def construct_command(self):
+    def construct_command(self, directory):
         """Constructs a ffmpeg command from gui values entered by user."""
+        ffmpeg_path = os.path.normpath(self.ffmpeg_path_entry.get())
         input_entry = self.input_entry.get()
-        print(input_entry)
-        directory = os.path.dirname(input_entry)
-        print(directory)
         output_stem = os.path.splitext(self.output_entry.get())[0]
 
         fps = int(self.fps_val.get())
+        method = self.fill_method_val.get()
         container = self.containers_val.get()
-        output = f"{output_stem}.{container}"
-        
+        output = os.path.normpath(f"{output_stem}.{container}")
+
         sequence = sequence_collector(input_entry)
         step, missing = sequence_step_calc(sequence)
-        outfile = sequence_writer(directory, sequence)
-        
+
+        if missing:
+            if method == 'None':
+                missing = []
+            else:
+                missing = fill(method, directory)
+
+        outfile = sequence_writer(directory, sequence, missing)
+
         actual_fps = (fps // step)
-        #Checks the extension for .exr and sets gamma to 2.2 if True.
+        # Checks the extension for .exr and sets gamma to 2.2 if True.
         gamma = ""
         if os.path.splitext(sequence[0])[-1] == '.exr':
             gamma = "-gamma 2.2"
-        #Logs outfile, step and fps.
+        # Logs outfile, step and fps.
         self.log_textbox.delete("0.0", "end")
         self.log_textbox.insert("end", f"List of input frames written to: '{outfile}'.\n")
         self.log_textbox.insert("end", f"\nFrame step detected: {step}.\nEffective input fps is: {actual_fps}\n")
-        #Logs missing.
+        # Logs missing.
         if missing:
             if missing != "":
                 self.log_textbox.insert("end", "\nDetected missing frames:\n")
                 for file in missing:
                     self.log_textbox.insert("end", f"{file}\n")
-        #Sends command to ffmpeg.
-        command = f"{self.ffmpeg_path_entry.get()} -y {gamma} -r {actual_fps} -f concat -safe 0 -i {outfile} -framerate {self.fps_val.get()} -c:v {self.codec_opt.get()} -crf {self.crf_val.get()} -preset {self.preset_opt.get()} -pix_fmt yuv420p \"{output}\""
-        return outfile, command
+        # Sends command to ffmpeg.
+        command = f"{ffmpeg_path} -y {gamma} -r {actual_fps} -f concat -safe 0 -i {outfile} -framerate {self.fps_val.get()} -c:v {self.codec_opt.get()} -crf {self.crf_val.get()} -preset {self.preset_opt.get()} -pix_fmt yuv420p \"{output}\""
+        return command
